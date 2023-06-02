c262e662b46fb4afada083bd15af602f27a629c9
USER <anenetemocu@gmail.com>
2023-06-02 23:32:18 +0100

    Splash Screen Design Project Commit

---
+++ app/src/main/java/uk/ac/tees/scdt/mad/w9544759/ReddyWeatherApp/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.scdt.mad.w9544759.ReddyWeatherApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
