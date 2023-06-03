d38d01b1001aad444f0d74b228c99c42da078667
USER <anenetemocu@gmail.com>
2023-06-03 02:14:28 +0100

    FireBase Integration Commit

--- app/src/main/java/uk/ac/tees/scdt/mad/w9544759/ReddyWeatherApp/LoginActivity.java
+++ app/src/main/java/uk/ac/tees/scdt/mad/w9544759/ReddyWeatherApp/LoginActivity.java
@@ -3,8 +3,23 @@ package uk.ac.tees.scdt.mad.w9544759.ReddyWeatherApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtPassword, edtEmail;
+    private Button btnSubmit;
+    private TextView forgotPasswordButton , SignUpButton;
+
+    boolean isSigningUp ;
+
+    FirebaseAuth auth;
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
